26f3aa1bf4b804a7b57ea81371cc45c6ca2d6937
ArnChristian007 <arnrosales321@gmail.com>
2025-08-12 21:15:38 +0800

    add

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,16 +1,45 @@
-import { CiMail, CiLock, CiFacebook, CiInstagram, CiTwitter } from "react-icons/ci";
-import { useState } from "react";
+import { CiMail, CiLock, CiUser, CiFacebook, CiInstagram, CiTwitter } from "react-icons/ci";
+import { useState, useEffect, useRef } from "react";
+import { animate } from "animejs";
 export default function Home() {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [register, setRegister] = useState(false);
+    const [check, setCheck] = useState(false);
+    const slide = useRef(null);
+    useEffect(() => {
+        if (!slide.current) return;
+        animate(slide.current, {
+            opacity: [0, 1],
+            translateX: [-100, 0],
+            duration: 1000,
+            ease: 'outCubic'
+        })
+    }, [register]);
+    console.log(check);
     return (
         <div className="min-h-screen flex items-center justify-between flex-col text-sm sm:text-base md:text-md lg:text-lg text-gray-700">
-            <div className="flex flex-1 items-center justify-center w-full px-3">
-                <div className="shadow p-5 border border-gray-300 rounded-lg w-100">
-                    <h1 className="font-semibold text-xl md:text-2xl lg:text-3xl">Sign In</h1>
-                    <p className="text-gray-500 mt-1">Welcome back! Please Sign In your account.</p>
+            <div className="flex flex-1 items-center justify-center w-full px-3 overflow-hidden">
+                <div ref={slide} className="shadow p-5 border border-gray-300 rounded-lg w-100">
+                    <h1 className="font-semibold text-xl md:text-2xl lg:text-3xl">
+                        {register ? 'Sign Up' : 'Sign In'}
+                    </h1>
+                    <p className="text-gray-500 mt-1">
+                        {register ? 'Fill up all requirements.' : 'Welcome back! Please Sign In your account.'}
+                    </p>
+                    {register && (
+                        <div className="flex items-center w-full border border-gray-400 p-2 rounded-md mt-3">
+                            <CiUser className="text-xl md:text-2xl lg:text-3xl"/>
+                            <input
+                                className="w-full outline-none ml-2"
+                                placeholder="Username"
+                                type="text"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                        </div>
+                    )}
                     <div className="flex items-center w-full border border-gray-400 p-2 rounded-md mt-3">
                         <CiMail className="text-xl md:text-2xl lg:text-3xl"/>
                         <input
@@ -32,16 +61,34 @@ export default function Home() {
                             onChange={(e) => setPassword(e.target.value)}
                         />
                     </div>
-                    <button className="mt-3 text-gray-500 hover:underline hover:decoration-0">
-                        Forgot Password?
+                    {!register ? (
+                        <button className="mt-3 text-gray-500 hover:underline focus:underline decoration-0">
+                            Forgot Password?
+                        </button>
+                    ) : (
+                        <div className="flex items-center mt-3 gap-2 text-gray-500">
+                            <input
+                                className="h-4 w-4 lg:w-5 lg:h-5"
+                                type="checkbox"
+                                checked={check}
+                                onChange={(e) => setCheck(e.target.checked)}
+                            />
+                            <label>Accept all terms & conditions.</label>
+                        </div>
+                    )}
+                    <button className="w-full bg-green-400 hover:bg-green-500 focus:bg-green-500 text-white p-2 rounded-md mt-3">
+                        {register ? 'Sign Up' : 'Sign In'}
                     </button>
-                    <button className="w-full bg-green-400 hover:bg-green-500 text-white p-2 rounded-md mt-3">
-                        Sign In
-                    </button>
-                    <div className="flex items-center mt-3 justify-center gap-2">
-                        <p className="text-gray-500">New user?</p>
-                        <button className="text-blue-400 hover:text-blue-500">Create New Account</button>
-                    </div>
+                    {!register ? (
+                        <div className="flex items-center mt-3 justify-center gap-2" onClick={() => setRegister(!register)}>
+                            <p className="text-gray-500">New user?</p>
+                            <button className="text-blue-400 hover:text-blue-500 focus:bg-blue-500">Create New Account</button>
+                        </div>
+                    ) : (
+                        <button className="w-full bg-blue-400 hover:bg-blue-500 focus:bg-blue-500 text-white p-2 rounded-md mt-3" onClick={() => setRegister(!register)}>
+                            Back
+                        </button>
+                    )}
                 </div>
             </div>
             <div className="bg-gray-700 p-5 w-full flex flex-col items-center justify-center text-white">
